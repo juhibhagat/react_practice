5b75880683c9a73d66cb6a36ee2b4024fb411c21
juhibhagat <jkri0206@gmail.com>
2026-01-31 19:25:58 +0530

    .

--- react_practice/src/router/index.jsx
+++ react_practice/src/router/index.jsx
@@ -12,16 +12,15 @@ function Important() {
                     <span className="mx-4 "><Link to="/">Home</Link></span>
                     <span className="mx-4"><Link to="javascript">javascript</Link></span>
                     <span className="mx-4"><Link to="mern">Mern</Link></span>
-                    <span className="mx-4"><Link to="python">Python</Link></span>
+                    <span className="mx-4"><Link to="/python">Python</Link></span>
                 </nav>
                 </header>
                 <section>
-                
-
                         <Routes>
                             <Route path="/" element={<div><h2>Shopping Home</h2> <p>Women fashion</p></div>}/>
                             <Route path="/javascript" element={<div><h2>JavaScript</h2></div>}/>
                             <Route path="/mern" element={<div><h2>merns</h2></div>}/>
+                             <Route path="/python" element={<div><h2>merns</h2></div>}/>
                         </Routes>
                 </section>
 
